47d5eecda648b72c2d9be20e39a27254cb872950
Lucas <lucass@gmail.com>
2015-05-16 21:43:48 -0300

    Depois da jogada mostra tabuleiro com peça.

--- src/quatronoburaco/Jogo.java
+++ src/quatronoburaco/Jogo.java
@@ -2,15 +2,26 @@ package quatronoburaco;
 
 public class Jogo {
 
+	private static final int JOGADOR1 = 1;
+	private static final int VAZIO = 0;
+	private int[][] tabuleiro = new int[6][7];
+	
 	public String getJogo() {
-		String expected = 
-				"-------\n"+
-				"-------\n"+
-				"-------\n"+
-				"-------\n"+
-				"-------\n"+
-				"-------";
-		return expected;
+		String tabuleiroAsString = "";
+		for (int i = 0; i < tabuleiro.length; i++) {
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				if(tabuleiro[i][j] == VAZIO)
+					tabuleiroAsString += "-";
+				if(tabuleiro[i][j] == JOGADOR1)
+					tabuleiroAsString += "X";
+			}
+			tabuleiroAsString += "\n";
+		}
+		return tabuleiroAsString;
+	}
+
+	public void jogar(int coluna) {
+		tabuleiro[5][coluna-1] = 1;
 	}
 
 }
--- tests/quatronoburaco/GameTests.java
+++ tests/quatronoburaco/GameTests.java
@@ -14,11 +14,45 @@ public class GameTests {
 				"-------\n"+
 				"-------\n"+
 				"-------\n"+
-				"-------";
+				"-------\n";
 		
 		Jogo subject = new Jogo();
 		String actual = subject.getJogo();
 		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void depoisDeUmaJogada_MostraTablueiroComPeca(){
+		String expected = 
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"X------\n";
+		
+		Jogo subject = new Jogo();
+		int coluna = 1;
+		subject.jogar(coluna);
+		String actual = subject.getJogo();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void depoisDeUmaJogadaDiferente_MostraTablueiroComPeca(){
+		String expected = 
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"-X-----\n";
+		
+		Jogo subject = new Jogo();
+		int coluna = 2;
+		subject.jogar(coluna);
+		String actual = subject.getJogo();
 		Assert.assertEquals(expected, actual);
 	}
 
