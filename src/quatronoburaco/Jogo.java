79aef61ff76ba8c686a2e297abe67cd9bd09eeac
Lucas <lucass@gmail.com>
2015-05-16 22:07:01 -0300

    Mostra tabuleiro depois de duas jogadas.

--- src/quatronoburaco/Jogo.java
+++ src/quatronoburaco/Jogo.java
@@ -3,8 +3,10 @@ package quatronoburaco;
 public class Jogo {
 
 	private static final int JOGADOR1 = 1;
+	private static final int JOGADOR2 =2;
 	private static final int VAZIO = 0;
 	private int[][] tabuleiro = new int[6][7];
+	private int vezDoJogador = JOGADOR1;
 	
 	public String getJogo() {
 		String tabuleiroAsString = "";
@@ -14,6 +16,8 @@ public class Jogo {
 					tabuleiroAsString += "-";
 				if(tabuleiro[i][j] == JOGADOR1)
 					tabuleiroAsString += "X";
+				if(tabuleiro[i][j] == JOGADOR2)
+					tabuleiroAsString += "O";
 			}
 			tabuleiroAsString += "\n";
 		}
@@ -21,7 +25,13 @@ public class Jogo {
 	}
 
 	public void jogar(int coluna) {
-		tabuleiro[5][coluna-1] = 1;
+		if(vezDoJogador == JOGADOR1){
+			tabuleiro[5][coluna-1] = JOGADOR1;
+			vezDoJogador = JOGADOR2;
+		}else{
+			tabuleiro[5][coluna-1] = JOGADOR2;
+			vezDoJogador = JOGADOR1;
+		}
 	}
 
 }
--- tests/quatronoburaco/GameTests.java
+++ tests/quatronoburaco/GameTests.java
@@ -55,5 +55,24 @@ public class GameTests {
 		String actual = subject.getJogo();
 		Assert.assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void depoisDeDuasJogadas_MostraTablueiroComPeca(){
+		String expected = 
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"-------\n"+
+				"XO-----\n";
+		
+		Jogo subject = new Jogo();
+		int colunaX = 1;
+		subject.jogar(colunaX);
+		int colunaO = 2;
+		subject.jogar(colunaO);
+		String actual = subject.getJogo();
+		Assert.assertEquals(expected, actual);
+	}
 
 }
